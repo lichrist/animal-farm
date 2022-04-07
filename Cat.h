966fd772870893261465637470ba735f471fd745
Christian <lichrist@hawaii.edu>
2022-04-07 09:09:48 -1000

    Updated addCats.h

---
+++ Cat.h
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_010d_animal_farm_2 - EE 205 - Spr 2022
+///
+/// @file Cat.h
+/// @version 1.0
+///
+/// @author Christian Li <lichrist@hawaii.edu>
+/// @date   03_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "config.h"
+#include "catDatabase.h"
+
+
+class Cat {
+protected:
+    //Protected
+    char name[MAXIMUM_CHAR_CAT];
+    enum Gender gender;
+    enum Breed breed;
+    bool isCatFixed;
+    Weight weight;
+public:
+    //Getters
+    const char *getName() const;
+    Gender getGender() const;
+    Breed getBreed() const;
+    bool isFixed() const;
+    Weight getWeight() const;
+
+    //Setters
+    void setName(const char* newName);
+    void setGender(Gender newGender);
+    void setBreed (Breed newBreed);
+    void fixCat();
+    void setWeight(Weight newWeight);
+
+    //Static methods
+    static bool validateName(const char* newName);
+    static bool validateGender(const Gender newGender);
+    static bool validateBreed(const Breed newBreed);
+    static bool validateWeight(const Weight newWeight);
+
+    //Public Member
+    Cat* next;
+
+    //Constructor
+    Cat();
+    Cat(const char *newName, const Gender newGender, const Breed newBreed, const Weight newWeight);
+
+
+    //Public Methods
+    bool print() const noexcept;
+    bool validate() const noexcept;
+
+private:
+    //Private Method Default
+    void zeroOutMemberData();
+};
+
+
+
