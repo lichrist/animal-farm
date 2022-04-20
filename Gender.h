5f64305598d97a0aa3f367742fed6fb09ce3a958
Christian <lichrist@hawaii.edu>
2022-04-19 15:39:20 -1000

    Added Gender header file

---
+++ Gender.h
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_010d_animal_farm_2 - EE 205 - Spr 2022
+///
+/// @file Gender.h
+/// @version 1.0
+///
+/// @author Christian Li <lichrist@hawaii.edu>
+/// @date   19_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include <iostream>
+#include "config.h"
+
+enum class Gender {
+    UNKNOWN_GENDER=0,
+    MALE,
+    FEMALE
+};
+
+inline std::ostream& operator<<( std::ostream& lhs_stream, const Gender& rhs_Gender ){
+    switch( rhs_Gender ) {
+        case Gender::UNKNOWN_GENDER:
+            lhs_stream << "Unknown gender";
+            break;
+        case Gender::MALE:
+            lhs_stream << "Male";
+            break;
+        case Gender::FEMALE:
+            lhs_stream << "Female";
+            break;
+        default:
+            /// @throw out_of_range If the enum is not mapped to a string.
+            throw std::out_of_range( PROGRAM_NAME ": Gender not mapped to a string" );
+    }
+    return lhs_stream;
+}
