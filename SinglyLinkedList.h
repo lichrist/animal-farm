b34045539f008c103b03a3c569a0f5748a9e9b60
Christian <lichrist@hawaii.edu>
2022-04-20 22:54:37 -1000

    Added SignlyLinkedList header

--- SinglyLinkedList.h
+++ SinglyLinkedList.h
@@ -8,8 +8,21 @@
 /// @author Christian Li <lichrist@hawaii.edu>
 /// @date   20_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
+#pragma once
 
-class SinglyLinkedList {
+#include "Node.h"
+#include "List.h"
 
+class SinglyLinkedList : public List {
+public:  ///////////////////////// Public Constructors /////////////////////////
+    SinglyLinkedList();        ///Make a new SinglyLinkedList
+
+public:  /////////////////////////// Public Methods ////////////////////////////
+    void  push_front( Node* newNode ) ;  ///Insert `newNode` to the beginning of the List
+    Node* pop_front() noexcept override; ///Remove and return the first Node in the List
+
+    void insert_after( Node* currentNode, Node* newNode ) ;  ///Insert `newNode` after `currentNode`
+
+    void dump() const noexcept override ;     ///Output the contents of this container
+    bool validate() const noexcept override ; ///Check to see if the container is valid
 };
-
