519ef716e5c7b586ee913448e971687193853dc8
Christian <lichrist@hawaii.edu>
2022-04-20 23:55:48 -1000

    Set methods

--- List.cpp
+++ List.cpp
@@ -9,4 +9,62 @@
 /// @date   20_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <cassert>
+#include "config.h"
 #include "List.h"
+
+bool List::empty() const noexcept {
+    if(head == nullptr){
+        return true;
+    }
+    return false;
+}
+
+unsigned int List::size() const noexcept {
+    return count;
+}
+
+bool List::isIn(Node *aNode) const {
+    Node* currentList = head;
+
+    while (currentList != nullptr)
+    {
+        if (currentList == aNode)
+            return true;
+        currentList = currentList->next;
+    }
+    return false;
+}
+
+bool List::isSorted() const noexcept {
+    if (head == nullptr) {
+        return true;
+    }
+    for (Node* head; head->next != nullptr; head = head->next) {
+        if (head > head->next){
+            return false;
+        }
+    }
+    return true;
+}
+
+Node* List::get_first() const noexcept {
+    if(!empty()){
+        return head;
+    }
+    return nullptr;
+}
+
+Node *List::get_next(const Node *currentNode) {
+    if (currentNode->next == nullptr){
+        throw std::invalid_argument("currentNode is nullptr.");
+    }
+    return currentNode->next;
+}
+
+void List::deleteAllNodes() noexcept {
+    while( head != nullptr ) {
+        pop_front();
+    }
+
+
