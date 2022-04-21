55f8bf0000423ff1baf4ba4018bdd5224df52cd2
Christian <lichrist@hawaii.edu>
2022-04-20 23:37:57 -1000

    Added Mammal header

--- Mammal.h
+++ Mammal.h
@@ -9,8 +9,42 @@
 /// @date   20_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 
-class Mammal {
+#pragma once
 
+#include "Animal.h"
+#include "Color.h"
+
+
+/// Mammals have more fun
+class Mammal : public Animal {
+public:   //////////////////////// Constants ///////////////////////////////////
+    static const std::string MAMMAL_NAME;  ///< The scientific name for Mammals
+
+protected:  ///////////////////////// Member Variables /////////////////////////
+    Color color = Color::UNKNOWN_COLOR;  ///< The primary color of the Mammal
+
+public:   /////////////////////// Constructors ////////////////////////////////
+    /// Create a Mammal with the *minimum* required fields
+    Mammal( const Weight::t_weight newMaxWeight
+            ,const std::string&     newSpecies
+    ) : Animal( newMaxWeight, MAMMAL_NAME, newSpecies ) {};
+
+    /// Create a Mammal, populating *all* of the member variables
+    Mammal( const Color            newColor
+            ,const Gender           newGender
+            ,const Weight::t_weight newWeight
+            ,const Weight::t_weight newMaxWeight
+            ,const std::string&     newSpecies
+    ) : Animal( newGender, newWeight, newMaxWeight, MAMMAL_NAME, newSpecies ) {
+        setColor( newColor );
+    };
+
+public:  ////////////////////////// Getters & Setters //////////////////////////
+    Color getColor() const noexcept { return color; }  ///< Get the color
+    void setColor( const Color newColor ) noexcept { color = newColor; }  ///< Set the color
+
+public:  /////////////////////////// Public Methods ////////////////////////////
+    void dump() const noexcept override;  ///< Print the contents of this object (and its parents)
 };
 
 
