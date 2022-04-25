9e546c971cd75275ce26b0772f6acbfcabb8f7f7
Christian <lichrist@hawaii.edu>
2022-04-24 21:32:36 -1000

    Updated to current main file

--- main.cpp
+++ main.cpp
@@ -11,26 +11,32 @@
 /// @date   03_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
-#include "addCats.h"
-#include "reportCats.h"
-#include "deleteCats.h"
 #include "Cat.h"
+#include "SinglyLinkedList.h"
 
 using namespace std;
 
-int main(){
-
-    cout << " Starting Animal Farm 2 " << endl;
-    addCat(new Cat("Loki", MALE, PERSIAN, 1.0));
-    addCat(new Cat("Milo", MALE, MANX, 1.1));
-    addCat(new Cat("Bella", FEMALE, MAINE_COON, 1.2));
-    addCat(new Cat("Kali", FEMALE, SHORTHAIR, 1.3));
-    addCat(new Cat("Trin", FEMALE, MANX, 1.4));
-    addCat(new Cat("Chili", MALE, SHORTHAIR, 1.5));
-    printAllCats();
-    deleteAllCats();
-    printAllCats();
-    cout << " Done with Animal Farm 2 " << endl;
+int main() {
+    cout << "Starting " << PROGRAM_TITLE << endl ;
+    SinglyLinkedList catDB ;
+    catDB.push_front( new Cat( "Loki", Color::CREAM, true, Gender::MALE, 1.0 ) ) ;
+    catDB.push_front( new Cat( "Milo", Color::BLACK, true, Gender::MALE, 1.1 ) ) ;
+    catDB.push_front( new Cat( "Bella", Color::BROWN, true, Gender::FEMALE, 1.2 ) ) ;
+    catDB.push_front( new Cat( "Kali", Color::CALICO, true, Gender::FEMALE, 1.3 ) ) ;
+    catDB.push_front( new Cat( "Trin", Color::WHITE, true, Gender::FEMALE, 1.4 ) ) ;
+    catDB.insert_after(catDB.get_first(), new Cat( "Chili", Color::GINGER, true,
+                                                   Gender::MALE, 1.5 ) );
+    for( Animal* pAnimal = (Animal*)catDB.get_first() ; pAnimal != nullptr ; pAnimal =
+                                                                                     (Animal*)List::get_next( (Node*)pAnimal ) ) {
+        cout << pAnimal->speak() << endl;
+    }
+    catDB.validate() ;
+    catDB.dump() ;
+    catDB.deleteAllNodes() ;
+    catDB.dump() ;
+    cout << "Done with " << PROGRAM_TITLE ;
+    return( EXIT_SUCCESS ) ;
+}
 
 #ifdef DEBUG
     Cat testCat = Cat();
@@ -133,4 +139,3 @@ int main(){
     return 0;
 #endif
 */
-}
